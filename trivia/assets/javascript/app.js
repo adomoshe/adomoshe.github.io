4868f7857f11d49d6de78c599f5f28fb7bd18b12
Ado <Ado@ados-macbook.lan>
2018-11-16 01:00:24 -0800

    Added trivia game

---
+++ trivia/assets/javascript/app.js
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+
+// <div>
+//                 <h3>What percent of American adults belive chocolate milk comes from brown cows?</h3>
+//                     <h6>7%</h6>
+//                     <h6></h6>
+//                     <h6></h6>
+//                     <h6></h6>
+//         </div>
+//         <div>
+//                 <h3>What is Bob Dylan's real name?</h3>
+//                     <h6>Robert Zimmerman</h6>
+//                     <h6></h6>
+//                     <h6></h6>
+//                     <h6></h6>
+//         </div>
+//         <div>
+//                 <h3>How many licks does it officialy take to get to the center of a Tootsie Pop?</h3>
+//                     <h6>364</h6>
+//                     <h6></h6>
+//                     <h6></h6>
+//                     <h6></h6>
+//         </div>
+//         <div>
+//             <h3>The CDC website has a section dedicated to?</h3>
+//                 <h6>Zombie prepardness</h6>
+//                 <h6></h6>
+//                 <h6></h6>
+//                 <h6></h6>
+//         </div>
+//         <div>
+//                 <h3>What does McDonald's refer to their frequent users as?</h3>
+//                     <h6>Heavy users</h6>
+//                     <h6></h6>
+//                     <h6></h6>
+//                     <h6></h6>
+//         </div>
+//         <div>
+//             <h3>What kills more people yearly than shark attacks?</h3>
+//                 <h6>Vending machines</h6>
+//                 <h6>Selfies</h6>
+//                 <h6>Coconuts</h6>
+//                 <h6>All of the above</h6>
+//         </div>
+//         <div>
+//             <h3>Under the Code of Hammurabi (Babylonian code of law in ancient Mesopotamia) bartenders that watered down beer were...</h3>
+//                 <h6>Punished by execution</h6>
+//                 <h6></h6>
+//                 <h6></h6>
+//                 <h6></h6>
+//             </div>
